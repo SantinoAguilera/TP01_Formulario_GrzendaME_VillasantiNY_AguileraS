614d5366b99d21c0dcac1446f63e6f4c30d9ae7e
SANTINO AGUILERA <48313116@ort>
2025-03-17 10:39:12 -0300

    Added TODO function verifyForm()

---
+++ script.js
@@ -0,0 +1,32 @@
+function verifyForm()
+{
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    let error = false;
+
+    if (name.trim().length === 0)
+    {
+        error = true;
+        console.log("ERRORnameHasNoValue")
+    }
+
+    if (email.trim().length === 0)
+    {
+        error = true;
+        console.log("ERRORemailHasNoValue")
+    }
+
+    if (password.trim().length === 0)
+    {
+        error = true;
+        console.log("ERRORpasswordHasNoValue")
+    }
+
+    if (confirmPassword.trim().length === 0)
+    {
+        error = true;
+        console.log("ERRORconfirmPasswordHasNoValue")
+    }
+}
