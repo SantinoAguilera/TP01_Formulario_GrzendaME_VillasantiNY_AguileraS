bbdb6d45cfc6d3bf08f7cf1467459a518e5a8a3d
Iamatoast <neuyv@protonmail.com>
2025-03-21 11:42:42 -0300

    Print Status added

--- script.js
+++ script.js
@@ -3,25 +3,34 @@ function darkModeToggle(){
     body.classList.toggle("dark-mode");
 }
 
-const printError = (input, subtext) =>{
-    let campo = getElementById(input);
-    let error = getElementById(subtext);
-
+const printStatus = (input, subtext, errorMsg, valid) =>{
+    let campo = document.getElementById(input);
+    let error = document.getElementById(subtext);
+    if (valid) {
+        campo.style.color = "green";
+        error.innerHTML = "";
+    } else {
+        campo.style.color = "red";
+        error.innerHTML = errorMsg;
+    }
 }
 
 function verifyPassword()
 {
     const password = document.getElementById("password").value;
     const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+    const errorMsg = "La contraseña debe de tener como mínimo 8 caracteres, al menos un número y una letra.";
+    let valid;
 
     if (console.log(regex.test(password)))
     {
-        //Añadir output grafico de que la contraseña esta bien (sugiero cambiarle el color al contorno del Input)
+        valid = true;
     }
     else
     {
-        document.querySelector("#errorPassword").innerHTML = "La contraseña debe de tener como mínimo 8 caracteres, al menos un número y una letra."
+        valid = false;
     }
+    printStatus("password", "errorPassword", errorMsg, valid);
 }
 
 function verifyForm()
