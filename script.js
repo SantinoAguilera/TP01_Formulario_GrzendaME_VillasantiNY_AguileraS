7222046656d5fab01a0c2bd9d5319c233aa3ca2c
SANTINO AGUILERA <48313116@ort>
2025-03-21 11:27:03 -0300

    Added verifyPassword() function to script.js

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ function darkModeToggle(){
     body.classList.toggle("dark-mode");
 }
 
+function verifyPassword()
+{
+    const password = document.getElementById("password").value;
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+
+    if (console.log(regex.test(password)))
+    {
+        //Añadir output grafico de que la contraseña esta bien (sugiero cambiarle el color al contorno del Input)
+    }
+    else
+    {
+        document.querySelector("#errorPassword").innerHTML = "La contraseña debe de tener como mínimo 8 caracteres, al menos un número y una letra."
+    }
+}
+
 function verifyForm()
 {
     const name = document.getElementById("name").value;
